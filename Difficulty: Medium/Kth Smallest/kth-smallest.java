a1a0337244a3b4c58eaab8f0e71e33cea2978a67
Lavanyalav25 <lavanyalavanya200525@gmail.com>
2026-07-25 10:14:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int kthSmallest(int[] arr, int k) {
+        // Code here
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            
+        }
+        return arr[k-1];
+    }
+}
+//7 10 4 3 20 15
+//3
+
+//3 4 7 10 15 20
+//7
